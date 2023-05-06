3248c06704537ec98cedc0a54695dcced508eced
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-05-06 21:48:32 +0200

    get the data from the mock API

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
+import NProgress from 'nprogress';
+import { mockData } from './mock-data';
 
 class NumberOfEvents extends Component {
-    state = {
-      numberOfEvents: 32,
-    };
-  
-    handleInputChange = (event) => {
-      const value = event.target.value;
-      this.setState({ numberOfEvents: value });
-    };
-  
-    render() {
-      return (
-        <div className="NumberOfEvents">
-          <input
-            type="number"
-            className="event"
-            value={this.state.numberOfEvents}
-            onChange={this.handleInputChange}
-          />
-          <ul className="AmountOfEvents">
-          </ul>
-        </div>
-      );
+  state = {
+    numberOfEvents: 32,
+    events: [],
+  };
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({ numberOfEvents: value });
+  };
+
+  getEvents = async () => {
+    NProgress.start();
+
+    if (window.location.href.startsWith("http://localhost")) {
+      NProgress.done();
+      return mockData;
     }
+  };
+
+  render() {
+    return (
+      <div className="NumberOfEvents">
+        <input
+          type="number"
+          className="event"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChange}
+        />
+        <ul className="AmountOfEvents">
+        </ul>
+      </div>
+    );
   }
-  
+}
 
 export default NumberOfEvents;
--- src/test/NumberOfEvents.test.js
+++ src/test/NumberOfEvents.test.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { mockData } from '../mock-data';
+import { getEvents } from '../api';
 import NumberOfEvents from '../NumberOfEvents';
-import App from '../App';
-import EventList from '../EventList';
 
 describe('<NumberOfEvents /> component', () => {
-    let NumberOfEventsWrapper;
+  let NumberOfEventsWrapper;
   
-    beforeAll(() => {
-      NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
-    });
-  
-    test('render number of events input', () => {
-      expect(NumberOfEventsWrapper.find('.event')).toHaveLength(1);
-    });
-  
-    test('render correct number of events', () => {
-      expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
-    });
-  
-    test('change number of events when input changes', () => {
-      const eventObject = { target: { value: 15 } };
-      NumberOfEventsWrapper.find('.event').simulate('change', eventObject);
-      NumberOfEventsWrapper.update();
-      expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(15);
-    });
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => {}} />);
   });
   
+  test('render number of events input', () => {
+    expect(NumberOfEventsWrapper.find('.event')).toHaveLength(1);
+  });
+  
+  test('render correct number of events', () => {
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+  });
+  
+  test('change number of events when input changes', () => {
+    const eventObject = { target: { value: 15 } };
+    NumberOfEventsWrapper.find('.event').simulate('change', eventObject);
+    NumberOfEventsWrapper.update();
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(15);
+  });
+  
+  test('get correct number of events from mock API', async () => {
+    const returnedEvents = await NumberOfEventsWrapper.instance().getEvents();
+    expect(returnedEvents).toEqual(mockData.slice(0));
+  });
+  
+});
