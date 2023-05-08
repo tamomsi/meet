ffa93320b905bf6089d66d8261e37c3d96b52c77
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-05-08 13:41:58 +0200

    fixed the number of events issue

--- src/test/NumberOfEvents.test.js
+++ src/test/NumberOfEvents.test.js
@@ -1,60 +1,53 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import App from '../App';
+import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 import { mockData } from '../mock-data';
-import { extractLocations, getEvents } from '../api';
+import { jest } from '@jest/globals';
 
 describe('<NumberOfEvents /> component', () => {
-  let NumberOfEventsWrapper;
+  let NumberOfEventsWrapper, mockUpdateEvents;
+  
   beforeAll(() => {
-    NumberOfEventsWrapper = mount(<NumberOfEvents updateEvents={() => {}} />);
+    mockUpdateEvents = jest.fn();
+    NumberOfEventsWrapper = shallow(<NumberOfEvents numberOfEvents={32} updateEvents={mockUpdateEvents} />);
   });
 
   test('render number of events input', () => {
-    expect(NumberOfEventsWrapper.find('.event')).toHaveLength(1);
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents input')).toHaveLength(1);
   });
 
-  test('render correct number of events', () => {
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+  test('render label for number of events input', () => {
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents label')).toHaveLength(1);
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents label').text()).toEqual('Number of Events:');
   });
 
-  test('change number of events when input changes', () => {
-    const eventObject = { target: { value: 15 } };
-    NumberOfEventsWrapper.find('.event').simulate('change', eventObject);
-    NumberOfEventsWrapper.update();
-    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(15);
+  test('render number of events input with default value', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents input').prop('value')).toEqual(numberOfEvents);
   });
 
-  test('get correct number of events from mock API', async () => {
+  test('update number of events state when input changes', () => {
+    const eventObject = { target: { value: 20 } };
+    NumberOfEventsWrapper.find('.NumberOfEvents input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual(20);
+  });
+
+  test('update events state in App component when input changes', () => {
+    const eventObject = { target: { value: 10 } };
+    NumberOfEventsWrapper.find('.NumberOfEvents input').simulate('change', eventObject);
+    expect(mockUpdateEvents).toHaveBeenCalledWith(null, 10);
+  });  
+
+  test('get data from API', async () => {
+    const mockEvents = mockData;
     const returnedEvents = await NumberOfEventsWrapper.instance().getEvents();
-    expect(returnedEvents).toEqual(mockData.slice(0));
-  });
-});
-
-describe('<App /> integration with NumberOfEvents', () => {
-  let AppWrapper;
-  beforeAll(() => {
-    AppWrapper = mount(<App />);
+    expect(returnedEvents).toEqual(mockEvents);
   });
 
-  test('update number of events when user changes input', async () => {
-    AppWrapper.instance().updateEvents = jest.fn();
-    AppWrapper.instance().forceUpdate();
-    const eventObject = { target: { value: 1 } };
-    const numberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
-    numberOfEventsWrapper.find('.event').simulate('change', eventObject);
-    expect(AppWrapper.state('numberOfEvents')).toBe(1);
-    const eventCount = mockData.length;
-    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(mockData.slice(0, eventCount));
-  });
-
-  test('render the correct number of events after user changes input', async () => {
-    const eventObject = { target: { value: 1 } };
-    const numberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
-    numberOfEventsWrapper.find('.event').simulate('change', eventObject);
-    AppWrapper.update();
-    const eventCount = mockData.slice(0, 1).length;
-    expect(AppWrapper.find('.Event')).toHaveLength(eventCount);
+  test('get data from API when URL starts with "http://localhost"', async () => {
+  const mockEvents = mockData;
+  window.location.href = 'http://localhost';
+  const returnedEvents = await NumberOfEventsWrapper.instance().getEvents();
+  expect(returnedEvents).toEqual(mockEvents);
   });
 });
