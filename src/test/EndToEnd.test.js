69df5fd997867df88cc202e7581aa7894630e98c
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-05-12 23:28:31 +0200

    fixed pupeteer issue and updated EndToEnd.test.js

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -4,10 +4,10 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
-    jest.setTimeout(30000);
+    jest.setTimeout(30000); // set timeout to 30 seconds
     browser = await puppeteer.launch();
     page = await browser.newPage();
-    page.goto('http://localhost:3000/');
+    await page.goto('http://localhost:3000/meet');
     await page.waitForSelector('.event');
   });
 
@@ -18,17 +18,18 @@ describe('show/hide an event details', () => {
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-  });
+  }, 30000);
 
   test('User can expand an event to see its details', async () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeDefined();
-  });
+  }, 30000);
 
   test('User can collapse an event to hide its details', async () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
-  });
+  }, 30000);
+
 });
