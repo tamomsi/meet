80aa57f381976d320ab3046e87af7bc8b6612f59
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-05-20 13:34:26 +0200

    added the 'Access-Control-Allow-Origin' header with the value 'https://tamomsi.github.io'

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -62,8 +62,8 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true, // Allow credentials
+          'Access-Control-Allow-Origin': 'https://tamomsi.github.io',
+          'Access-Control-Allow-Credentials': true,
         },
         body: JSON.stringify(token),
       };
