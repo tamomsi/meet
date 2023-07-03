3c5b4d5139dc1ee779289650f4868a64ec156137
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-07-03 14:40:38 +0200

    implemented ErrorAlert

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
-import { InfoAlert } from './Alert';
+import { InfoAlert, ErrorAlert } from './Alert';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    infoAlertText: '', // New state for the info alert text
+    infoAlertText: '',
+    errorAlertText: '', // New state for the error alert text
   };
 
   updateEvents = (location, eventCount) => {
@@ -45,21 +46,32 @@ class App extends Component {
 
   handleInputChanged = (inputValue) => {
     this.setState({
-      infoAlertText: inputValue, // Set the info alert text in the state
+      infoAlertText: inputValue,
+    });
+  };
+
+  handleError = (errorMessage) => {
+    this.setState({
+      errorAlertText: errorMessage,
     });
   };
 
   render() {
     return (
       <div className="App">
-        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+          onError={this.handleError} // Pass onError prop
+        />
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
-          onInputChanged={this.handleInputChanged} // Pass onInputChanged prop
+          onInputChanged={this.handleInputChanged}
         />
         <div className="alerts-container">
           {this.state.infoAlertText && <InfoAlert text={this.state.infoAlertText} />}
+          {this.state.errorAlertText && <ErrorAlert text={this.state.errorAlertText} />}
         </div>
         <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents} />
       </div>
