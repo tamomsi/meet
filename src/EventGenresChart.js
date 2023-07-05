8c76d5f94dc0caed3f1cd513faaa6857f50abc07
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-07-05 22:59:42 +0200

    added grid

--- src/EventGenresChart.js
+++ src/EventGenresChart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Cell, Label } from 'recharts';
 
 const EventGenresChart = ({ events }) => {
   const [data, setData] = useState([]);
@@ -21,6 +21,19 @@ const EventGenresChart = ({ events }) => {
     setData(getData());
   }, [events]);
 
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="#8884d8" textAnchor="middle" dominantBaseline="central">
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
   // Colors for the pie chart segments
   const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8'];
 
@@ -32,12 +45,13 @@ const EventGenresChart = ({ events }) => {
           dataKey="value"
           fill="#8884d8"
           labelLine={false}
-          label
+          label={renderCustomizedLabel}
           outerRadius={130}
         >
           {data.map((entry, index) => (
             <Cell key={index} fill={colors[index % colors.length]} />
           ))}
+          <Label value="Event Genres" position="center" fontSize={16} />
         </Pie>
       </PieChart>
     </ResponsiveContainer>
