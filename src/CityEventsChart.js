a585ab0361815ecbfde2c2517b42e36e291ddd58
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-07-05 22:00:02 +0200

    changed margin bottom and left

--- src/CityEventsChart.js
+++ src/CityEventsChart.js
@@ -19,7 +19,7 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const formattedData = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length;
-      const city = location.split(', ')[0];
+      const city = location.split((/, | - /))[0];
       return { city, count };
     });
     return formattedData;
@@ -31,8 +31,8 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 60,
+          left: -30,
         }}
       >
         <CartesianGrid />
